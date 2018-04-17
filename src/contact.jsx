4c3ebe4cbdf2b0e955f3f48d539eedc79131c9f9
Shiratie Prodhan <shiratiee@gmail.com>
2018-04-16 22:30:14 -0400

    social media icons actually work now

--- src/contact.jsx
+++ src/contact.jsx
@@ -10,8 +10,12 @@ export default function Contact() {
       <h4 className="contact">
       Want to get in touch? Email me at shiratiee@gmail.com
       </h4>
-      <img src={github}/>
-      <img src={linkedin}/>
+      <a href="https://github.com/">
+      <img src={github} alt="github" />
+    </a>
+    <a href="https://www.linkedin.com/in/shiratie-prodhan/">
+    <img src={linkedin} alt="linkedin" />
+  </a>
     </div>
   );
 }
