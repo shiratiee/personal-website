7712da870b8e787b66487f3a18d026056d1f444f
Shiratie Prodhan <shiratiee@gmail.com>
2018-04-18 14:55:26 -0400

    made some changes to about me

--- src/About.jsx
+++ src/About.jsx
@@ -13,7 +13,7 @@ export default function About() {
       <p className="my-blurb">
       I'm a developer based in New York City working mainly with JavaScript. 
       I have expertise throughout the tech stack but I am most excited about the front-end. 
-      Currently, I'm leveling up my CSS/Sass skills and am interested in learning new technologies like Mobile Development, Python and Go.
+      Currently, I'm leveling up my CSS/Sass skills and am interested in learning new technologies like Mobile Development, Python and C#.
       When I'm not coding, I love being in the outdoors and using makeup as an art form!
       </p>
     </div>
