a942765238b72b0e75af50694727f2b89b24bb1d
Shiratie Prodhan <shiratiee@gmail.com>
2018-04-21 16:10:20 -0400

    making the about me look cleaner

--- src/About.jsx
+++ src/About.jsx
@@ -4,17 +4,17 @@ import Me from './images/Me.png'
 
 export default function About() {
   return (
-
     <div>
     <img 
      className="my-image"
      src={Me}
-     alt="Me"/>
+     alt="Me"
+     />
       <p className="my-blurb">
-      I'm a developer based in New York City working mainly with JavaScript. 
-      I have expertise throughout the tech stack but I am most excited about the front-end. 
-      Currently, I'm leveling up my CSS/Sass skills and am interested in learning new technologies like Mobile Development, Python and C#.
-      When I'm not coding, I love being in the outdoors and using makeup as an art form!
+        I'm a developer based in New York City working mainly with JavaScript. 
+        I have expertise throughout the tech stack but I am most excited about the front-end. 
+        Currently, I'm leveling up my CSS/Sass skills and am interested in learning new technologies like Mobile Development, Python and C#.
+        When I'm not coding, I love being in the outdoors and using makeup as an art form!
       </p>
     </div>
   );
