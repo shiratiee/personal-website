5bccbc9ece92617c98e2aa9cbeace39ad13b8a22
Shiratie Prodhan <shiratiee@gmail.com>
2018-04-23 16:05:35 -0400

    made my main image smaller and the font on my about me larger

--- src/About.jsx
+++ src/About.jsx
@@ -11,12 +11,12 @@ export default function About() {
      src={Me}
      alt="Me"
      />
-      <p className="my-blurb">
+      <h2 className="my-blurb">
         I'm a developer based in New York City working mainly with JavaScript. 
         I have expertise throughout the tech stack but I am most excited about the front-end. 
         Currently, I'm leveling up my CSS/Sass skills and am interested in learning new technologies like Mobile Development, Python and C#.
         When I'm not coding, I love being in the outdoors and using makeup as an art form!
-      </p>
+      </h2>
     </div>
   );
 }
