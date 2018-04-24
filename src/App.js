d8bfd46fc9644961ed43e0957361a025a26624c2
Shiratie Prodhan <shiratiee@gmail.com>
2018-04-24 15:53:21 -0400

    changing routes again

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
       <Router history={history}>
       <div className="App">
       <Navbar/>
-        <Route exact path="/About" component={About} />
+        <Route exact path="/about" component={About} />
         <Route exact path="/skills" component={Skills} />
         <Route exact path="/projects" component={Projects} />
         <Route exact path="/contact" component={Contact} />
