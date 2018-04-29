5d1ff3a77825c1c604152a915ab95f7e422a611d
Shiratie Prodhan <shiratiee@gmail.com>
2018-04-29 15:43:28 -0400

    tweeked about me

--- src/About.jsx
+++ src/About.jsx
@@ -14,9 +14,9 @@ export default function About() {
       <h2 className="my-blurb">
         I'm a developer based in New York City working mainly with JavaScript. 
         I have expertise throughout the tech stack but I am most excited about the front-end. 
-        Currently, I'm leveling up my CSS/Sass skills and am interested in learning new technologies like Mobile Development, Python and C#.
+        Currently, I'm leveling up my CSS/Sass skills and am interested in learning new technologies including Mobile Development, Python and C#.
         <br/>
-        Aside from coding, I love art & music. I love being in the outdoors and gaming on my phone! 
+        Aside from writing coding, I love art & music. I love being in the outdoors and gaming on my phone! 
       </h2>
     </div>
   );
