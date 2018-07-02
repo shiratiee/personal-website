d7882408806ea5df2df34dcb3e21a759e6040a1c
Shiratie Prodhan <shiratiee@gmail.com>
2018-07-02 17:18:14 -0400

    minor change

--- src/About.jsx
+++ src/About.jsx
@@ -14,7 +14,7 @@ export default function About() {
       <h2 className="my-blurb">
         I'm a developer based in New York City working mainly with JavaScript. 
         I have skills throughout the tech stack but I am most excited about the front-end. 
-        Currently, I'm leveling up my CSS/Sass skills and am interested in learning new technologies including Python and Apollo/GraphQL.
+        Currently, I'm leveling up my CSS/SASS skills and am interested in learning new technologies including Python and Apollo/GraphQL.
         <br/>
       </h2>
     </div>
