e6b1923c924798d9038f60c306cb838a4896954e
Shiratie Prodhan <shiratiee@gmail.com>
2018-09-04 21:16:42 -0400

    under construction

--- src/navbar.js
+++ src/navbar.js
@@ -4,27 +4,28 @@ import { Link } from 'react-router-dom'
 export default function Navbar () {
 
     return (
-      <div>
-      <div className ="my-name" style={{ textDecoration: 'none' }}>
-      <Link to="/" style={{ textDecoration: 'none' }}>
-        <h1></h1>
-      </Link>
-    </div>
-        <div className="nav">
-          <Link to="/about">
-            <h3>About</h3>
-          </Link>
-          <Link to="/skills">
-            <h3>Skills</h3>
-          </Link>
-          <Link to="/projects">
-            <h3>Projects</h3>
-          </Link>
-          <Link to="/contact">
-          <h3>Contact</h3>
-        </Link>
-        </div>
+      <h2></h2>
+    //   <div>
+    //   <div className ="my-name" style={{ textDecoration: 'none' }}>
+    //   <Link to="/" style={{ textDecoration: 'none' }}>
+    //     <h1></h1>
+    //   </Link>
+    // </div>
+    //     <div className="nav">
+    //       <Link to="/about">
+    //         <h3>About</h3>
+    //       </Link>
+    //       <Link to="/skills">
+    //         <h3>Skills</h3>
+    //       </Link>
+    //       <Link to="/projects">
+    //         <h3>Projects</h3>
+    //       </Link>
+    //       <Link to="/contact">
+    //       <h3>Contact</h3>
+    //     </Link>
+    //     </div>
 
-      </div>
+    //   </div>
     )
 }
